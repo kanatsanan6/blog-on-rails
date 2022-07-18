8ddc7e9cdbe5b5c641709b74c51793d4360b0551
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-18 10:23:21 +0700

    test: add test for article models

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe 'valid factory' do
+    subject { build(:article) }
+
+    it { is_expected.to be_valid }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :body }
+    it { should validate_length_of(:body).is_at_least(10).on(:create) }
+  end
+end
